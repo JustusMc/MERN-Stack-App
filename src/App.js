04972bb34b8a6254ac233f91216a1894c9131562
JustusMc <dev@justusm.com>
2021-09-07 12:18:44 -0700

    Main component file set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import US
+
+function App() {
+  return <Router>
+    <Route path='/'>
+
+    </Route>
+  </Router>;
+}
+
+export default App;
